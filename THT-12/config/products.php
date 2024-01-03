dd582a24b5067aafb6698635784dab35f028d38e
kesh15 <rakeshbramantyo123@gmail.com>
2024-01-03 21:40:12 +0700

    Push THT-12

---
+++ THT-12/config/products.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'name' => 'Dapper Line',
+        'price' => 1000,
+        'description' => 'Tampil dengan gaya yang elegan dan kasual sekaligus dalam kemeja lengan panjang ini.',
+        'image' => 'https://dynamic.zacdn.com/I7t2V1ksCZNqj6GSOk77cY4WpSQ=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/cotton-on-kids-7037-4151273-1.jpg'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Urbane Threads',
+        'price' => 5999000,
+        'description' => 'Didesain dengan bahan katun berkualitas tinggi yang memberikan kenyamanan.',
+        'image' => 'https://dynamic.zacdn.com/sE-mbZ2oG_-kQmBojkODoH9p-XY=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/marks-spencer-0414-5918324-3.jpg'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Modish Attire',
+        'price' => 4999000,
+        'description' => 'Hoodie yang stylish dan nyaman untuk menemani aktifitas sehari-hari',
+        'image' => 'https://dynamic.zacdn.com/0p8yPp13kwNhGeoeBaxcE2YN6Lo=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/marks-spencer-6710-5573824-1.jpg'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Masculine Moda',
+        'price' => 3999000,
+        'description' => 'Jaket denim yang stylish ini merupakan perpaduan sempurna antara gaya klasik dan modern',
+        'image' => 'https://dynamic.zacdn.com/TWj3Xon9I1q4Y3z9Vsu580quhos=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/marks-spencer-2874-2583824-1.jpg'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Supreme Maneuvre',
+        'price' => 2999000,
+        'description' => 'Kemeja flanel yang hangat dan nyaman untuk dipakai di musim dingin',
+        'image' => 'https://dynamic.zacdn.com/M7QeBEfkva5MxTk-fVHP6pioJtk=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/marks-spencer-7382-3428324-3.jpg'
+    ],
+    [
+        'id' => 6,
+        'name' => 'Prestige Males',
+        'price' => 1999000,
+        'description' => 'Kemeja slim-fit dengan desain minimalis yang menawan. Cocok dipadukan dengan blazer',
+        'image' => 'https://dynamic.zacdn.com/fug2Xq2iBiPozQlgc9gUb0Cypqw=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/hummel-5979-2841224-1.jpg'
+    ],
+    [
+        'id' => 7,
+        'name' => 'Elite Ensemble',
+        'price' => 7999000,
+        'description' => 'Kemeja linen yang ringan dan nyaman dipakai di musim panas.',
+        'image' => 'https://dynamic.zacdn.com/ckOuLFcbmHykkxWXr0lLFyYw6iA=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/puma-5885-3140904-1.jpg'
+    ],
+    [
+        'id' => 8,
+        'name' => 'Exude Elegance',
+        'price' => 9999000,
+        'description' => 'Kemeja formal dengan pola garis halus yang menambahkan dimensi pada tampilan Anda.',
+        'image' => 'https://dynamic.zacdn.com/3dDyn_W9MQINgLDvpCZHCfhXAQs=/filters:quality(70):format(webp)/https://static-id.zacdn.com/p/adidas-5447-6539643-6.jpg'
+    ],
+];
